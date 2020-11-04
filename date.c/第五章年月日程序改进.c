a161928572c05038b2ba4e72fe9dcd0365f18bf0
qznn <72787839+qznn@users.noreply.github.com>
2020-11-04 22:04:45 +0800

    Update 第五章年月日程序改进.c

--- date.c/第五章年月日程序改进.c
+++ date.c/第五章年月日程序改进.c
@@ -45,7 +45,7 @@ int main(void)
 		case 0:printf("Monday");break;
 		case 1:printf("Tuesday");break;
 		case 2:printf("Wednesday");break;
-		case 3:printf("Thusday");break;
+		case 3:printf("Thursday");break;
 		case 4:printf("Friday");break;
 		case 5:printf("Saturday");break;
 		case 6:printf("Sunday");break;
