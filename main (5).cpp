4619b189ba469f5fdcee843e8920b217b924e255
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:54:48 +0800

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+int wordsearch(char ch,char str[])
+{
+    int nlength=strlen(str);
+    int nwordcount=0;
+    int ncount=0;
+    for(int i=0;i<nlength;i++)
+    {
+       if (ch==str[i])
+        {
+           ncount++;
+        }
+      if(str[i]==' '||str[i]==','||str[i]=='.')
+      {
+          if(ncount>0)
+          {
+              nwordcount++;
+              ncount=0;
+          }
+      }
+
+    }
+    return nwordcount;
+}
+int main()
+{
+    int ncount=0;
+    char ch;
+    char str[100];
+    cin>>ch;
+    cin.get();
+    cin.getline(str,100);
+    int nlength=strlen(str);
+     for(int i=0;i<nlength;i++)
+     {
+         if(ch==str[i])
+            ncount++;
+     }
+
+    cout<<ncount<<" "<<wordsearch(ch,str);
+
+    return 0;
+}
+
+
+
