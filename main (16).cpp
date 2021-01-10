2d14f43d9b944f40d56e67d00934e3abdc9264e0
qznn <72787839+qznn@users.noreply.github.com>
2021-01-11 00:01:10 +0800

    Add files via upload

---
+++ main (16).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int a,b,c;
+   char ch;
+   cin>>a>>b>>ch;
+   while(1)
+   {  if(a==0&&b==0) break;
+      else
+     switch(ch)
+      {
+         case '+':c=a+b;cout<<c<<endl;break;
+         case '-':c=a-b;cout<<c<<endl;break;
+         case '*':c=a*b;cout<<c<<endl;break;
+         case '/':
+           {if(b!=0)
+              {c=a/b;cout<<c<<endl;break;}
+            else {cout<<"Error"<<endl;break;}
+           }
+         case '%':c=a%b;cout<<c<<endl;break;
+         default:cout<<"Error"<<endl;
+      }
+      cin>>a>>b>>ch;
+   }
+
+return 0;
+}
+
