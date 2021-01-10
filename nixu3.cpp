59fdba4b990b88ae26087933ac367378fde71f75
qznn <72787839+qznn@users.noreply.github.com>
2021-01-11 00:06:38 +0800

    Add files via upload

---
+++ nixu3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e,s;
+    cin>>a;
+    b=a/1000;
+    c=a%1000/100;
+    d=a%1000%100/10;
+    e=a%10;
+    s=e*1000+d*100+c*10+b;
+    cout <<s<< endl;
+    return 0;
+}
+
