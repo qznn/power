2949ef067c3c7919752f5ca1bf2275c29adad305
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:58:13 +0800

    Add files via upload

---
+++ main (11).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n,a1,a2,b1,b2,m=0;
+   for(n=1000;n<=2000;n++)
+   {
+       a1=n/1000;
+       a2=(n-a1*1000)/100;
+       b1=n%100/10;
+       b2=n%10;
+       if(a1+a2==b1+b2)
+       {
+          m=m+1;
+          cout<<n<<" ";
+       }
+   }
+    cout << endl;
+    cout <<m<<endl;
+    return 0;
+}
+
