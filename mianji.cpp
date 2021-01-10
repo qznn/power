1c0d051cb06ba15fece8a666ce9a51a982659dd6
qznn <72787839+qznn@users.noreply.github.com>
2021-01-11 00:06:19 +0800

    Add files via upload

---
+++ mianji.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  float a,b,c,x,area;
+  cin>>a>>b>>c;
+  if(a+b>c&&a+c>b&&b+c>a)
+  {
+      x=(a+b+c)/2;
+      area=sqrt(x*(x-a)*(x-b)*(x-c));
+      cout<<fixed<<setprecision(3)<<area<<endl;
+  }
+    else cout <<"ERROR" << endl;
+    return 0;
+}
+
