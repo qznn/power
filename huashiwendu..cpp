83f42e02c57e35411821761f6708cd606e3beb56
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:52:55 +0800

    Add files via upload

---
+++ huashiwendu..cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    float F,C;
+    cin>>F>>C;
+    if(F!=32)
+    {
+      C=5.0/9*(F-32);
+    cout<<fixed<<setprecision(4)<<C<<endl;
+    }
+
+    else cout <<"0"<< endl;
+    return 0;
+}
+
+
