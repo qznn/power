1f8ed14b5a6c4b5868847e24b12bb163a991f936
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:51:32 +0800

    Add files via upload

---
+++ ack.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int i;
+int Ack(int m,int n)
+{
+    if(m==0&&n>0)   {i++; return n+1; }
+    if(n==0&&m>0)   {i++; return Ack(m-1,1);}
+    if(m>0&&n>0)    {i++; return Ack(m-1,Ack(m,n-1));}
+}
+int main()
+{
+     int m,n;
+     cin>>m>>n;
+     cout<<Ack(m,n)<<endl;
+     cout<<"nAckNum="<<i;
+    return 0;
+}
+
