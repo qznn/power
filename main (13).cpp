c2015bb654e8152c52689f2bcb5c98fb24ee4b65
qznn <72787839+qznn@users.noreply.github.com>
2021-01-11 00:00:10 +0800

    Add files via upload

---
+++ main (13).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=0,j=0,n;
+    cin>>n;
+    while(n!=0)
+       {if(n>0)
+         i++;
+        if(n<0)
+         j++;
+         cin>>n;
+       }
+    cout<<"positive integers are "<<i;
+    cout<<" and negetive integers are "<<j<<endl;
+    return 0;
+}
+
+
