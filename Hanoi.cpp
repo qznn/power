cfa56930c89e6ce3c3aa4a7888aacbd11d99bdd5
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:52:34 +0800

    Add files via upload

---
+++ Hanoi.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int count;
+void Move(int n,char a,char b)
+{
+  count++;
+  cout<<count<<":"<<" Move "<<n<<" from "<<a<<" to "<<b<<endl;
+}
+void Hanoi(int n,char a,char b,char c)
+{
+    if(n==1)
+      {
+          Move(n,a,b);
+      }
+    else
+     {
+       Hanoi(n-1,a,c,b);
+       Move(n,a,b);
+       Hanoi(n-1,c,b,a);
+     }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    Hanoi(n,'A','B','C');
+    return 0;
+}
+
