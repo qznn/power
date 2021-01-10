0dd2fc695e8763e5d4e4a5e6e58d80f198469531
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:50:37 +0800

    Add files via upload

---
+++ 2.flower (1).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int fun(int i)
+{
+  int a,b,c;
+     a=i/100;
+     b=i%100/10;
+     c=i%10;
+  if(i==a*a*a+b*b*b+c*c*c)
+    return 1;
+  else return 0;
+}
+int main()
+{  int m,n,a;
+   cin>>m>>n;
+   cout<<"Input m: "<<"Input n: ";
+    for(a=m;a<=n;a++)
+      {
+        if(fun(a)==1) cout<<a<<endl;
+      }
+    return 0;
+}
+
+
