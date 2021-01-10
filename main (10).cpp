6c1a66fdaa580c25a27fb4f06b29fe464efb572c
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:56:32 +0800

    Add files via upload

---
+++ main (10).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int scores;
+  cin>>scores;
+  switch(scores/10)
+   {case 9:cout<<"A"<<endl; break;
+    case 8:cout<<"B"<<endl; break;
+    case 7:cout<<"C"<<endl; break;
+    case 6:cout<<"D"<<endl; break;
+    default:cout<<"E"<<endl; break;
+   }
+    return 0;
+}
+
