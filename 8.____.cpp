62fe92c20f63537540e87cb7636ac21a28fad20e
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:51:09 +0800

    Add files via upload

---
+++ 8.____.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a,b,c;
+   int n;
+    cin>>a>>b>>c;
+    bool flag=false;
+    n=10;
+    while(n<=100)
+    {
+        if(n%3==a && n%5==b && n%7==c)
+        {
+            cout<<n<<endl;
+            flag=true;
+            break;
+        }
+        n++;
+    }
+    if(n>100) cout << "No answer!" << endl;
+    return 0;
+}
+
