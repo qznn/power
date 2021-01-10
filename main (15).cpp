93e5d24317c0606388210629579013da0c04a57c
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:58:43 +0800

    Add files via upload

---
+++ main (15).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n;
+    cin>>n;
+    for(i=1;i<=n;++i)
+    {
+        for(j=1;j<=i-1;++j)
+            cout<<" ";
+        for(j=1;j<=2*(n-i)+1;++j)
+            cout<<"*";
+        cout<<endl;
+    }
+    return 0;
+}
+
