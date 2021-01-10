ce8e942a8b2825e50529b5738dd9b91d1499b6fb
qznn <72787839+qznn@users.noreply.github.com>
2021-01-10 23:54:11 +0800

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+int read(int a[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+        cin>>a[i];
+        return 0;
+}
+void sort(int date[],int n)
+{
+    int i,j,k,temp;
+    for(i=0;i<n;i++)
+    {
+        k=i;
+        for(j=k+1;j<n;j++)
+        {
+            if (date[j]<date[i]);
+            k=j;
+        }
+            if(k!=i)
+            {
+                temp=date[k];
+                date[k]=date[i];
+                date[i]=temp;
+            }
+    }
+
+}
+int date(int date[],int n,int b[],int len)
+{
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        b[len]=date[i];
+        len++;
+        for(j=i+1;j<n;j++)
+        {
+            if (date[j]!=date[i])
+                break;
+           else i++,j++;
+        }
+    }
+}
+int print(int a[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+        cout<<a[i]<<" ";
+}
+int main()
+{
+    int a[20],b[20];
+    read(a,20);
+    sort(a,20);
+    date(a,20,b,20);
+    print(a,20);
+    return 0;
+}
+
+
+
+
